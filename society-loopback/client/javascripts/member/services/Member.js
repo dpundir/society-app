fe4ff65c4c0568ce84a6b54beca2e7eaab529298
Pundir, Deepak <pundir.friend@gmail.com>
2015-12-20 15:33:57 +0530

    added comment

--- society-loopback/client/javascripts/member/services/Member.js
+++ society-loopback/client/javascripts/member/services/Member.js
@@ -1,38 +1,42 @@
 define([
-    'angular'
-],function() {
-    angular.module("societyApp.member")
-        .service('MemberService',['$http','$q', 'restInterface',function ($http, $q, restInterface) {
-            this.defaultMember = function defaultMember(){
-                return {
-                    fname: '',
-                    mname:'',
-                    lname: '',
-                    phone: '',
-                    ffname: '',
-                    flanme: '',
-                    fmname: '',
-                    dob:''
-                }
-            };
-            this.defaultMemberAddress = function defaultMemberAddress(){
-                return {
-                    address1: '',
-                    address2: '',
-                    address3: '',
-                    city: '',
-                    state: '',
-                    pin: ''
-                }
-            };
-            this.register = function register(){
+  'angular'
+], function () {
+  angular.module("societyApp.member")
+    .service('MemberService', ['$http', '$q', 'restInterface', function ($http, $q, restInterface) {
+      this.defaultMember = function defaultMember() {
+        return {
+          fname: '',
+          mname: '',
+          lname: '',
+          phone: '',
+          ffname: '',
+          flanme: '',
+          fmname: '',
+          dob: ''
+        }
+      };
+      this.defaultMemberAddress = function defaultMemberAddress() {
+        return {
+          address1: '',
+          address2: '',
+          address3: '',
+          city: '',
+          state: '',
+          pin: ''
+        }
+      };
+      this.register = function register() {
 
-            };
-            this.list = function(filter){
-              filter = angular.merge(filter || {}, {"filter":
-                {"include":["address","depositHistory"]}
-              });
-              return restInterface.get('/api/Members', null, filter);
-            }
-        }])
+      };
+      this.list = function (filter) {
+        //default filter to include address and deposit history data in member
+        //this relation is defined in member.json
+        var defaultMemberListFilter = {
+          "filter": {
+            "include": ["address", "depositHistory"]}
+        };
+        filter = angular.merge(filter || {}, defaultMemberListFilter);
+        return restInterface.get('/api/Members', null, filter);
+      }
+    }])
 });

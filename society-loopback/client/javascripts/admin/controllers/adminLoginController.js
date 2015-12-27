92b54f06fcba906a28a6d3f3661ee14ae7c8f22a
Pundir, Deepak <pundir.friend@gmail.com>
2015-12-27 15:09:54 +0530

    login page label corrected

--- society-loopback/client/javascripts/admin/controllers/adminLoginController.js
+++ society-loopback/client/javascripts/admin/controllers/adminLoginController.js
@@ -1,34 +1,37 @@
 define([
-    'angular',
-    'javascripts/common/services/authentication'
+  'angular',
+  'javascripts/common/services/authentication'
 ], function () {
-    angular.module("societyApp.admin.controller.login", [])
-        .controller('adminLoginController', ['$scope', '$location', '$routeParams', 'AuthenticationService', function ($scope, $location, $routeParams, AuthenticationService) {
-            $scope.actionText = 'Login';
+  angular.module("societyApp.admin.controller.login", [])
+    .controller('adminLoginController', ['$scope', '$location', '$routeParams', 'AuthenticationService', function ($scope, $location, $routeParams, AuthenticationService) {
+      $scope.actionText = 'Login';
+      $scope.headerText = 'Login';
 
-            var action = $routeParams.action;
-            if (action === 'reset') {
-                $scope.actionText = 'Reset';
-            } else if (action === 'register') {
-                $scope.actionText = 'Register';
-            }
-            $scope.adminLogin = function(){
-                var authenticationData = {"password": $scope.password};
-              if(action !== 'reset') {
-                if ($scope.username.indexOf('@') > 0) {
-                  authenticationData.email = $scope.username;
-                } else {
-                  authenticationData.username = $scope.username;
-                }
-              }
-                var action = $routeParams.action;
-                if (action === 'reset') {
-                    AuthenticationService.requestResetPassword(authenticationData);
-                } else if (action === 'register') {
-                    AuthenticationService.register(authenticationData);
-                } else {
-                    AuthenticationService.authenticate(authenticationData);
-                }
-            };
-        }]);
+      var action = $routeParams.action;
+      if (action === 'reset') {
+        $scope.actionText = 'Reset';
+        $scope.headerText = 'Reset Password';
+      } else if (action === 'register') {
+        $scope.actionText = 'Register';
+        $scope.headerText = 'Register User';
+      }
+      $scope.adminLogin = function () {
+        var authenticationData = {"password": $scope.password};
+        if (action !== 'reset') {
+          if ($scope.username.indexOf('@') > 0) {
+            authenticationData.email = $scope.username;
+          } else {
+            authenticationData.username = $scope.username;
+          }
+        }
+        var action = $routeParams.action;
+        if (action === 'reset') {
+          AuthenticationService.requestResetPassword(authenticationData);
+        } else if (action === 'register') {
+          AuthenticationService.register(authenticationData);
+        } else {
+          AuthenticationService.authenticate(authenticationData);
+        }
+      };
+    }]);
 });

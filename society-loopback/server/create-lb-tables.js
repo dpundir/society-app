4b837c4c38e081441dd1240315706926a9d3231c
Pundir, Deepak <pundir.friend@gmail.com>
2015-12-29 23:01:44 +0530

    user-member integration

--- society-loopback/server/create-lb-tables.js
+++ society-loopback/server/create-lb-tables.js
@@ -5,7 +5,8 @@ var server = require('./server');
 var ds = server.dataSources['mysql-ds'];
 var lbTables1 = ['Member', 'Address', 'Loan', 'M2mMemberLoan', 'MemberAddressView', 'MemberDeposit',
   'SocietyConfig', 'SocietyExpense', 'TransactionHistory'];
-var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role'];
+var lbTables2 = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role'];
+var lbTables = ['user'];
 ds.autoupdate(lbTables, function(er) {
   if (er) throw er;
   console.log('Looback tables [' + lbTables + '] created in ', ds.adapter.name);

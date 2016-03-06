352cf8ef834cdaf3ec183ade2be801ab9754d6bb
Mishra, Debashis <Debashis.Mishra@Kronos.com>
2016-03-06 21:38:14 +0530

    Document upload

--- society-loopback/client/javascripts/member/directives/memberDirectives.js
+++ society-loopback/client/javascripts/member/directives/memberDirectives.js
@@ -275,35 +275,108 @@ define([
                 link:function(){}
             }
         }])
-        .directive('memberDocument',['$filter', '$parse', function ($filter, $parse) {
+        .directive('memberDocument',['$filter', '$parse', 'fileUpload', function ($filter, $parse, fileUpload) {
             return{
                 restrict: 'A',
                 scope:{
                     member:'=',
-                    clickHandler:'&'
+                    clickHandler:'&',
+                    documents: '='
                 },
-                controller: ['$scope', 'fileUpload',function($scope, fileUpload){
-                    $scope.uploadFile = function(){
-                        var file = $scope.myFile;
-                        var uploadUrl = "/file/upload";//"/api/Documents/upload";
-                        fileUpload.uploadFileToUrl($scope.member.id, file, uploadUrl);
-                    };
-                    $scope.fetchDocumentList = function(){
-                        fileUpload.fetchDocumentList($scope.member.id);
-                    };
-                    $scope.fetchDocument = function(){
-                        fileUpload.fetchDocument($scope.member.id, 'Payment_Details.pdf');
-                    };
+                controller: ['$scope',function($scope, fileUpload){
+                    $scope.documents = $scope.documents || {};
+                    $scope.successMsg = "Fils(s) uploaded successfully.";
+                    $scope.errorMsg = "";
+                    $scope.showSuccessMsg = false;
+                    $scope.showErrorMsg = false;
+                    $scope.documents.successCB=successCB;
+                    $scope.documents.errorCB=errorCB;
+                    $scope.isUploadDocumentCollapsed = true;
+                    $scope.isDocumentCollapsed = false;
+                    function successCB(data){
+                        $scope.showSuccessMsg = false;
+                        $scope.showErrorMsg = false;
+                        $scope.documents.list = [];
+                        _.forEach(data, function(document){
+                            $scope.documents.list.push({
+                                name: document,
+                                href: "/file/" + $scope.member.id + "/download/" + document
+                            })
+                        });
+                        console.log(data);
+                    }
+                    function errorCB(error){
+                        console.log(error);
+                    }
+                    $scope.documentsList = [];
+
                 }],
                 templateUrl:'javascripts/member/partials/memberDocument.html',
                 link:function(scope, element, attrs){
-                    var model = $parse(element.find('input').attr('file-data'));
-                    var modelSetter = model.assign;
-
-                    element.bind('change', function(){
-                        scope.$apply(function(){
-                            modelSetter(scope, element.find('input')[0].files[0]);
+                    scope.fileQueue = [];
+                    var allowedFileType = {
+                        'application/pdf': true,
+                        'image/jpeg': true,
+                        'image/png': true
+                    },maxSize = 1024*1024;
+                    var fileInput = element.find('input');
+                    scope.uploadFile = function(file){
+                        fileUpload.uploadFileToUrl(scope.member.id, scope.files[file.index], "/file/upload").then(function(success){
+                            _.remove(scope.fileQueue, function(files) {
+                                return file.index === files.index;
+                            });
+                            if(!scope.fileQueue.length){
+                                fileInput[0].value = '';
+                            }
+                            scope.showSuccessMsg = true;
+                        },function(error){
+                            scope.errorMsg = 'Error in file upload, please try again later.'
                         });
+                    };
+                    scope.fetchDocument = function(document){
+                        fileUpload.fetchDocument(scope.member.id, document);
+                    };
+                    scope.uploadAllFile = function(){
+
+                    };
+                    scope.removeFile = function(index){
+                        scope.fileQueue.splice(index,1);
+                        if(!scope.fileQueue.length){
+                            fileInput[0].value = '';
+                        }
+                    };
+                    fileInput.bind('change', function(){
+                        scope.files = fileInput[0].files;
+                        scope.showErrorMsg = false;
+                        if(scope.files.length >3){
+                            scope.errorMsg = 'Maximum file allowed for upload is 3.';
+                            scope.showErrorMsg = true;
+                            fileInput[0].value = '';
+                            scope.$digest();
+                            return;
+                        }
+                        _.forEach(scope.files, function(file,index){
+                            if(!(file.type in allowedFileType)){
+                                scope.errorMsg = 'Invalid file type, allowed are pdf, image/jpeg, image/png, file name :' + file.name;
+                                scope.showErrorMsg = true;
+                                fileInput[0].value = '';
+                                scope.$digest();
+                                return;
+                            }
+                            if(file.size > maxSize){
+                                scope.errorMsg = 'Max allowed file size is 1MB. file name :' + file.name;
+                                scope.showErrorMsg = true;
+                                fileInput[0].value = '';
+                                scope.$digest();
+                                return;
+                            }
+                            scope.fileQueue.push({
+                                name: file.name,
+                                type: file.type,
+                                index: index
+                            });
+                        });
+                        scope.$digest();
                     });
                 }
             }

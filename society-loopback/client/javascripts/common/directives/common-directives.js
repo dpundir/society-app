5df64dc285ee595645420793e55ef61cf8001920
Pundir, Deepak <pundir.friend@gmail.com>
2016-08-04 23:36:36 +0530

    nav-bar search by id implemented

---
+++ society-loopback/client/javascripts/common/directives/common-directives.js
@@ -0,0 +1,47 @@
+define(['angular', 'bootstrap'], function(angular){
+    'use strict';
+    angular.module("societyApp.common.directives.commonDirectives", ['ui.bootstrap'])
+        .directive('messageModal', function($uibModal) {
+            return {
+                restrict: 'EA',
+                scope: {
+                    message: '='
+                },
+                controller: function($scope){
+                    var modalInstance;
+                    $scope.primaryAction = function () {
+                        modalInstance.close();
+                    };
+
+                    $scope.secondaryAction = function () {
+                        modalInstance.dismiss('cancel');
+                    };
+                    $scope.message = angular.extend({
+                        type: 'information',
+                        showPrimary: true,
+                        showSecondary: true,
+                        textHeader: 'Message',
+                        textPrimary: 'Ok',
+                        textSecondary: 'Cancel'
+                    }, $scope.message);
+
+                    $scope.message.showPrimary = !!$scope.message.showPrimary;
+                    $scope.message.showSecondary = !!$scope.message.showSecondary;
+
+                    if($scope.message.type == 'information' || $scope.message.type == 'error'){
+                        $scope.message.showSecondary = false;
+                    }
+
+                    $scope.message.openModal = function () {
+                        modalInstance = $uibModal.open({
+                            animation: true,
+                            templateUrl: 'javascripts/common/partials/message-modal.html',
+                            backdrop:'true',
+                            size:'lg',
+                            scope:$scope
+                        });
+                    };
+                }
+            };
+        });
+});
--- society-loopback/client/javascripts/home/directive/navigationBar.js
+++ society-loopback/client/javascripts/home/directive/navigationBar.js
@@ -1,15 +1,19 @@
 define([
     'angular',
-    'javascripts/common/services/authentication'
+    'javascripts/common/services/authentication',
+    'javascripts/common/directives/common-directives'
 ], function () {
-    angular.module("societyApp.home.directive.navigationbar", ['ngCookies', 'societyApp.common.services.authentication'])
-        .directive('navBar', ['$location', '$cookies', 'AuthenticationService', function ($location, $cookies, AuthenticationService) {
+    angular.module("societyApp.home.directive.navigationbar", ['ngCookies',
+        'societyApp.common.services.authentication',
+        'societyApp.common.directives.commonDirectives'])
+        .directive('navBar', ['$location', '$cookies', 'AuthenticationService', 'restInterface', function ($location, $cookies, AuthenticationService, restInterface) {
             return{
                 restrict: 'A',
                 scope: {
                     navBarConfig: '=config'
                 },
                 controller: ['$scope', function ($scope) {
+                    $scope.message = {};
                     $scope.$watch(function() {
                         var user = $cookies.getObject('user');
                         return user? (user.username? user.username: user.email) : null;
@@ -24,7 +28,19 @@ define([
                     };
                     $scope.userProfile = function () {
                         $location.url('/userprofile/view');
-                    }
+                    };
+                    $scope.searchMemberById = function(){
+                        restInterface.get('/api/Members/'+$scope.memberId+'/exists').then(
+                            function(data){
+                                if(data.exists) {
+                                    $location.url('/member/view/' + $scope.memberId);
+                                } else{
+                                    $scope.message.message = "Member Id:" + $scope.memberId + " Not Found";
+                                    $scope.message.openModal();
+                                }
+                                $scope.memberId = undefined;
+                            });
+                    };
                 }],
                 templateUrl: 'javascripts/home/partials/nav-bar.html',
                 link: function () {

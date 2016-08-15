383be7eb162dd2ce5c8380c13ed95fb85f8c1327
Pundir, Deepak <pundir.friend@gmail.com>
2016-08-15 19:49:10 +0530

    role update changes

--- society-loopback/client/javascripts/settings/controllers/manageUserController.js
+++ society-loopback/client/javascripts/settings/controllers/manageUserController.js
@@ -10,9 +10,17 @@ define([
         'societyApp.common.services.restinterface',
         'societyApp.admin.services.userprofile'
     ])
-        .controller('manageUserController', ['$scope', '$location', '$routeParams', '$filter', 'restInterface', 'gridService', 'SelectOptions',
-            function ($scope, $location, $routeParams, $filter, restInterface, gridService, SelectOptions) {
-                $scope.msg = {isCellEdited: false};
+        .controller('manageUserController', ['$scope', '$location', '$q', '$routeParams', '$filter', 'restInterface', 'gridService', 'SelectOptions',
+            function ($scope, $location, $q, $routeParams, $filter, restInterface, gridService, SelectOptions) {
+                function resetController(){
+                    $scope.msg.isCellEdited = false;
+                    $scope.msg.lastCellEdited = '';
+                    $scope.editedUserData = [];
+                    $scope.manageUserGrid.selectedRowId = 0;
+                    $scope.manageUserGrid.selectedRowName = '';
+                }
+
+                $scope.msg = {};
 
                 $scope.manageUserGrid = gridService.getDefaultGridConfig([
                         {field: 'id'},
@@ -49,32 +57,38 @@ define([
                     var request = _.differenceWith($scope.editedUserData, $scope.userData, _.isEqual);
                     if(request.length > 0) {
                         var changedUserData = _.find($scope.userData, {id: request[0].id});
+                        var promises = [];
                         if (request[0].status !== changedUserData.status) {
-                            restInterface.update('/api/users/' + changedUserData.id, {status: request[0].status}).then(function (data) {
+                            var updateUser = restInterface.update('/api/users/' + changedUserData.id, {status: request[0].status}).then(function (data) {
                                 console.log(data);
                             });
+                            promises.push(updateUser);
                         }
                         if (!(request[0].role && changedUserData.role) || (request[0].role && changedUserData.role && request[0].role.role.id !== changedUserData.role.role.id)) {
+                            var updateRole;
                             if(changedUserData.role){
-                                restInterface.update('/api/users/change-role', {
+                                updateRole = restInterface.update('/api/users/change-role', {
                                     roleId: request[0].role.role.id,
                                     userId: changedUserData.id
                                 }).then(function (data) {
                                     console.log(data);
                                 });
                             } else{
-                                restInterface.post('/api/users/change-role', {
+                                updateRole = restInterface.post('/api/users/change-role', {
                                     roleId: request[0].role.role.id,
                                     userId: changedUserData.id
                                 }).then(function (data) {
                                     console.log(data);
                                 });
                             }
+                            promises.push(updateRole);
                         }
+                        $q.all(promises).then(function(){
+                            $scope.list();
+                        });
                     }
                 };
-                $scope.showHistory = function showHistory(name) {
-                }
+
                 $scope.list = function list() {
                     var filter = {
                         "filter": {
@@ -84,6 +98,7 @@ define([
                     return restInterface.get('/api/users/list', null, filter).then(function (data) {
                         $scope.manageUserGrid.data = data;
                         $scope.userData = _.cloneDeep(data);
+                        resetController();
                         $scope.gridApi.core.handleWindowResize();
                     });
                 };

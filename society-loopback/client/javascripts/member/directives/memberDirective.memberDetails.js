ddaf12fba69c4a56faf0058a90aff9b836b6472d
Pundir, Deepak <pundir.friend@gmail.com>
2016-12-24 21:31:08 +0530

    phone number validation added

--- society-loopback/client/javascripts/member/directives/memberDirective.memberDetails.js
+++ society-loopback/client/javascripts/member/directives/memberDirective.memberDetails.js
@@ -4,120 +4,126 @@
 define([
     'angular',
     'lodash',
-    'angular-bootstrap-lightbox'
+    'angular-bootstrap-lightbox',
+	'javascripts/common/services/validation-service'
 ], function (angular, _) {
-    angular.module("societyApp.member.directives.memberDetails", ['bootstrapLightbox', "societyApp.member.services.member"])
-        .directive('memberDetails', ['fileUpload', 'SelectOptions', 'Lightbox', function (fileUpload, SelectOptions, Lightbox) {
-            return {
-                restrict: 'A',
-                scope: {
-                    person: '=',
-                    entity: '@',
-                    address: '=',
-                    isViewMode: '=',
-                    actionText: '=',
-                    clickHandler: '&',
-                    editable: '='
-                },
-                controller: ['$scope', function ($scope) {
-                    /*
-                     * @method
-                     * @name validateRegistrationForm
-                     * */
-                    function validateRegistrationForm(form) {
-                        if (form.$invalid) {
-                            var formFields = ['first_name', 'last_name', 'father_first_name', 'father_last_name', 'mother_first_name', 'mother_last_name', 'gender', 'maritalStatus', 'phone', 'dob', 'address1', 'address2', 'city', 'state', 'pincode','guardianType'];
-                            _.each(formFields, function (name) {
-                                if (form[name].$invalid) {
-                                    form[name].$setTouched();
-                                }
-                            });
-                            return false;
-                        } else {
-                            return true;
-                        }
-                    }
-
-                    $scope.isInfoCollapsed = false;
-                    $scope.isAddressCollapsed = false;
-                    $scope.maritalStatuses = SelectOptions.getMaritalStatusOptions();
-                    $scope.genderOptions = SelectOptions.getGenderOptions();
-                    $scope.statusOptions = SelectOptions.getPersonStatusOptions();
-
-                    /*
-                     * Default date picker config
-                     * @type object
-                     * */
-                    $scope.dob = {
-                        dateOption: {
-                            formatYear: 'yy',
-                            maxDate: new Date(),
-                            startingDay: 1
-                        },
-                        format: 'dd-MM-yyyy',
-                        status: {
-                            opened: false
-                        }
-                    };
-                    $scope.open = function open() {
-                        this.dob.status.opened = true;
-                    };
-                    $scope.register = function (form) {
-                        var isFormValid = true;
-                        if (this.$parent.mode !== 2 && this.$parent.$parent.mode !== 2) {
-                            isFormValid = validateRegistrationForm(form)
-                        }
-                        if (isFormValid) {
-                            $scope.clickHandler({form: form, entity: $scope.entity});
-                        }
-                    };
-                    /*
-                     * Save a new deposit entry of a member,
-                     * */
-                    $scope.addEditProfilePhoto = function () {
-                        var isAddProfilePhoto = !$scope.person.profilePhotoName;
-						fileUpload.addEditProfilePhoto($scope.person.id, $scope.file, isAddProfilePhoto).then(function(data) {
-							$scope.person.profilePhotoName = data.fileName;
-						}, function(error) {
-							console.log(error);
+    angular.module("societyApp.member.directives.memberDetails", [
+		'bootstrapLightbox',
+		"societyApp.member.services.member",
+		'societyApp.common.services.validationService'
+	])
+	.directive('memberDetails', ['fileUpload', 'SelectOptions', 'Lightbox', 'validationService', function (fileUpload, SelectOptions, Lightbox, validationService) {
+		return {
+			restrict: 'A',
+			scope: {
+				person: '=',
+				entity: '@',
+				address: '=',
+				isViewMode: '=',
+				actionText: '=',
+				clickHandler: '&',
+				editable: '='
+			},
+			controller: ['$scope', function ($scope) {
+				/*
+				 * @method
+				 * @name validateRegistrationForm
+				 * */
+				function validateRegistrationForm(form) {
+					if (form.$invalid) {
+						var formFields = ['first_name', 'last_name', 'father_first_name', 'father_last_name', 'mother_first_name', 'mother_last_name', 'gender', 'maritalStatus', 'phone', 'dob', 'address1', 'address2', 'city', 'state', 'pincode','guardianType'];
+						_.each(formFields, function (name) {
+							if (form[name].$invalid) {
+								form[name].$setTouched();
+							}
 						});
-                    };
-                    /*
-                     * Save a new deposit entry of a member
-                     * */
-                    $scope.deleteProfilePhoto = function () {
-                        fileUpload.deleteProfilePhoto($scope.person.id, $scope.person.profilePhotoName).then(function (data) {
-                            $scope.person.profilePhotoName = undefined;
-                        }, function (error) {
-                            console.log(error);
-                        })
-                    };
-                    $scope.setGuardianType = function(type){
-                        $scope.person.guardianType = Number(type);
-                    };
+						return false;
+					} else {
+						return true;
+					}
+				}
 
-                    $scope.openLightboxModal = function(){
-                        if($scope.person.profilePhotoName){
-                            var imageArray = [
-                                {
-                                    'url': '/file/'+ $scope.person.id + '/document/' + $scope.person.profilePhotoName,
-                                    'thumbUrl': '/file/'+ $scope.person.id + '/document/' + $scope.person.profilePhotoName
-                                }
-                            ];
-                            Lightbox.openModal(imageArray, 0);
-                        }
-                    }
+				$scope.isInfoCollapsed = false;
+				$scope.isAddressCollapsed = false;
+				$scope.maritalStatuses = SelectOptions.getMaritalStatusOptions();
+				$scope.genderOptions = SelectOptions.getGenderOptions();
+				$scope.statusOptions = SelectOptions.getPersonStatusOptions();
+				$scope.phonePattern = validationService.getPhoneNumberPattern();
 
-                }],
-                templateUrl: 'javascripts/member/partials/memberDetails.html',
-                link: function (scope, element) {
-                    var fileInput = element.find('input#profilePhotoUpload');
+				/*
+				 * Default date picker config
+				 * @type object
+				 * */
+				$scope.dob = {
+					dateOption: {
+						formatYear: 'yy',
+						maxDate: new Date(),
+						startingDay: 1
+					},
+					format: 'dd-MM-yyyy',
+					status: {
+						opened: false
+					}
+				};
+				$scope.open = function open() {
+					this.dob.status.opened = true;
+				};
+				$scope.register = function (form) {
+					var isFormValid = true;
+					if (this.$parent.mode !== 2 && this.$parent.$parent.mode !== 2) {
+						isFormValid = validateRegistrationForm(form)
+					}
+					if (isFormValid) {
+						$scope.clickHandler({form: form, entity: $scope.entity});
+					}
+				};
+				/*
+				 * Save a new deposit entry of a member,
+				 * */
+				$scope.addEditProfilePhoto = function () {
+					var isAddProfilePhoto = !$scope.person.profilePhotoName;
+					fileUpload.addEditProfilePhoto($scope.person.id, $scope.file, isAddProfilePhoto).then(function(data) {
+						$scope.person.profilePhotoName = data.fileName;
+					}, function(error) {
+						console.log(error);
+					});
+				};
+				/*
+				 * Save a new deposit entry of a member
+				 * */
+				$scope.deleteProfilePhoto = function () {
+					fileUpload.deleteProfilePhoto($scope.person.id, $scope.person.profilePhotoName).then(function (data) {
+						$scope.person.profilePhotoName = undefined;
+					}, function (error) {
+						console.log(error);
+					})
+				};
+				$scope.setGuardianType = function(type){
+					$scope.person.guardianType = Number(type);
+				};
 
-                    fileInput.bind('change', function () {
-                        scope.file = fileInput[0].files[0];
-                        scope.addEditProfilePhoto();
-                    });
-                }
-            }
-        }]);
+				$scope.openLightboxModal = function(){
+					if($scope.person.profilePhotoName){
+						var imageArray = [
+							{
+								'url': '/file/'+ $scope.person.id + '/document/' + $scope.person.profilePhotoName,
+								'thumbUrl': '/file/'+ $scope.person.id + '/document/' + $scope.person.profilePhotoName
+							}
+						];
+						Lightbox.openModal(imageArray, 0);
+					}
+				}
+
+			}],
+			templateUrl: 'javascripts/member/partials/memberDetails.html',
+			link: function (scope, element) {
+				var fileInput = element.find('input#profilePhotoUpload');
+
+				fileInput.bind('change', function () {
+					scope.file = fileInput[0].files[0];
+					scope.addEditProfilePhoto();
+				});
+			}
+		}
+	}]);
 });
